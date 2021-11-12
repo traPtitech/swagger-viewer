b87ec44699adb91a856d2d9e7b60fc6e748d8079
sapphi-red <green@sapphi.red>
2021-11-12 11:47:14 +0900

    refactor urls.js

--- src/urls.js
+++ src/urls.js
@@ -1,46 +1,49 @@
+const github = (repo, path, branch = 'master') =>
+  `https://raw.githubusercontent.com/traPtitech/${repo}/${branch}/${path}`
+
 export default [
   {
     name: 'traQ v3 API',
-    url: 'https://raw.githubusercontent.com/traPtitech/traQ/master/docs/v3-api.yaml'
+    url: github('traQ', 'docs/v3-api.yaml')
   },
   {
     name: 'traQ v2 API',
-    url: 'https://raw.githubusercontent.com/traPtitech/traQ/master/docs/swagger.yaml'
+    url: github('traQ', 'docs/swagger.yaml')
   },
   {
     name: 'booQ',
-    url: 'https://raw.githubusercontent.com/traPtitech/booQ/master/docs/swagger.yml'
+    url: github('booQ', 'docs/swagger.yml')
   },
   {
     name: 'anke-to',
-    url: 'https://raw.githubusercontent.com/traPtitech/anke-to/master/docs/swagger/swagger.yaml'
+    url: github('anke-to', 'docs/swagger/swagger.yaml', 'main')
   },
   {
     name: 'knoQ',
-    url: 'https://raw.githubusercontent.com/traPtitech/knoQ/master/docs/swagger.yaml'
+    url: github('knoQ', 'docs/swagger.yaml')
   },
   {
     name: 'Jomon v2 API',
-    url: 'https://raw.githubusercontent.com/traPtitech/Jomon/v2/docs/swagger.yaml'
+    url: github('Jomon', 'docs/swagger.yaml', 'v2')
   },
   {
     name: 'Jomon v1 API',
-    url: 'https://raw.githubusercontent.com/traPtitech/Jomon/master/docs/swagger.yaml'
+    url: github('Jomon', 'docs/swagger.yaml')
   },
   {
     name: 'traPortfolio',
-    url: 'https://raw.githubusercontent.com/traPtitech/traPortfolio/master/docs/swagger/traPortfolio.v1.yaml'
+    url: github('traPortfolio', 'docs/swagger/traPortfolio.v1.yaml', 'main')
   },
   {
     name: 'traP Collection',
-    url: 'https://raw.githubusercontent.com/traPtitech/trap-collection-server/develop/docs/swagger/openapi.yml'
+    url: github('trap-collection-server', 'docs/swagger/openapi.yml', 'develop')
   },
   {
     name: 'NeoShowcase',
-    url: 'https://raw.githubusercontent.com/traPtitech/NeoShowcase/master/api/http/swagger.yaml'
+    url: github('NeoShowcase', 'api/http/swagger.yaml', 'main')
   },
   {
     name: 'Emoine',
-    url: 'https://raw.githubusercontent.com/traPtitech/Emoine/master/docs/openapi.yml'
+    url: github('Emoine', 'docs/openapi.yaml')
   }
 ]
