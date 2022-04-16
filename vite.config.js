4d674f7bf981fbb00ef877e3e1495064dcfaee25
sapphi-red <green@sapphi.red>
2022-04-17 03:32:09 +0900

    Revert "fix config load"
    This reverts commit e7d1d6ca598434de6f57067c7ff1ac166682fd65.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
+import { defineConfig } from 'vite'
 
-export default {
+export default defineConfig({
   build: {
     rollupOptions: {
       output: {
@@ -24,4 +25,4 @@ export default {
       }
     }
   }
-}
+})
