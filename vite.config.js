7a8956dd4fc5911f08e498f10a0277b59aad9c3c
sapphi-red <green@sapphi.red>
2022-10-18 19:53:39 +0900

    fix preview error

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,13 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  optimizeDeps: {
+    disabled: false
+  },
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: id => {
-          if (id.includes('/swagger-ui/')) {
-            return 'core'
-          }
-          if (id.includes('/swagger-client/')) {
-            return 'swagger-client'
-          }
-          if (id.includes('/autolinker/') || id.includes('/remarkable') || id.includes('/dompurify/') || id.includes('xml-but-prettier') || id.includes('js-yaml')) {
-            return 'library-misc'
-          }
-          if (id.includes('/react') || id.includes('/redux') || id.includes('/immutable') || id.includes('/prop-types/')) {
-            return 'react'
-          }
-          if (id.includes('/lodash') || id.includes('core-js') || id.includes('babel')) {
-            return 'lodash-and-babel'
-          }
-          return null
-        }
-      }
-    }
-  }
+    minify: false,
+    commonjsOptions: {
+      include: [],
+    },
+  },
 })
